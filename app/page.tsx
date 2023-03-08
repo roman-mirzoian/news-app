8235fafbd6296753d6725386b8bf214ebc25f1ea
Roman Mirzoian <roman.mirzoian@plus4u.net>
2023-03-08 02:09:18 +0200

    feat:     add new types for response from server function,     update common view mode,     install graphql

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import "../styles/globals.css"; 
+import "../styles/globals.css";
 
-export default function Homepage() {
+import { categories } from "../constants";
+import fetchNews from "../lib/fetchNews";
+
+async function Homepage() {
+  const news: NewsResponse = await fetchNews(categories.join(","));
+
   return <div>Homepage</div>;
 }
+
+export default Homepage;
